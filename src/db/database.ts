ea97fce8cd28d338aa5f2b043cbaa6194ed9eb6b
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-06-02 17:24:50 +0700

    new db setup

--- src/db/database.ts
+++ src/db/database.ts
@@ -4,7 +4,37 @@ import * as schema from "./schema";
 import { env } from "@/env";
 
 const pool = createPool({
-  uri: env.DATABASE_URL,
+  // uri: env.DATABASE_URL!,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "bid-buddy",
 });
 
 export const db = drizzle(pool, { schema, mode: "default" });
+
+// import { env } from "@/env";
+// import * as schema from "./schema";
+// import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
+// import postgres from "postgres";
+
+// declare global {
+//   // eslint-disable-next-line no-var -- only var works here
+//   var database: PostgresJsDatabase<typeof schema> | undefined;
+// }
+
+// let database: PostgresJsDatabase<typeof schema>;
+// let pg: ReturnType<typeof postgres>;
+
+// if (env.NODE_ENV === "production") {
+//   pg = postgres(env.DATABASE_URL);
+//   database = drizzle(pg, { schema });
+// } else {
+//   if (!global.database) {
+//     pg = postgres(env.DATABASE_URL);
+//     global.database = drizzle(pg, { schema });
+//   }
+//   database = global.database;
+// }
+
+// export { database, pg };
