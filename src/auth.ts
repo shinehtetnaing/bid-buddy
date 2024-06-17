55584a64f4ab6155ea4c058b0ce966fe83adc4bf
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-06-17 18:16:20 +0700

    update next config and next-auth

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,19 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { db } from "./db/database";
 import { users, accounts, sessions, verificationTokens } from "./db/schema";
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db, {
@@ -11,5 +24,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     sessionsTable: sessions,
     verificationTokensTable: verificationTokens,
   }),
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
   providers: [Google],
 });
